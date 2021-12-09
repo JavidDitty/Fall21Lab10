b133298f941dee7d43fa61a51b558f9930de6d43
Javid Ditty <jditty@umich.edu>
2021-12-08 19:40:54 -0500

    Adding test statements to testPostive() and testNegative()

--- TestSelectionSort.java
+++ TestSelectionSort.java
@@ -36,12 +36,31 @@ public class TestSelectionSort {
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
 
-        /** add tests to check for this unit test **/
+        SelectionSort sort = new SelectionSort();
+        int[] resultArr = sort.basicSelectionSort(arr);
+        Assert.assertNotNull("Failure: Actual value is null but expected value is not null", resultArr);
+        Assert.assertArrayEquals("Failure: Actual value not equal to expected value", Sortedarr, resultArr);
     }
 
     public void testNegative(){
+    	int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
 
-        /** Test data contains negative values only **/
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
+
+        SelectionSort sort = new SelectionSort();
+        int[] resultArr = sort.basicSelectionSort(arr);
+        Assert.assertNotNull("Failure: Actual value is null but expected value is not null", resultArr);
+        Assert.assertArrayEquals("Failure: Actual value not equal to expected value", Sortedarr, resultArr);
     }
 
     public void testMixed(){
